3628c09020c23314fe8ae998758f1428509015e3
Trushang Patel <TrushMac@Trushangs-MacBook-Pro.local>
2020-04-16 22:23:20 +0530

    project init

---
+++ Exercise/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by Trushang Patel on 16/04/20.
+//  Copyright © 2020 Trushangpatel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
